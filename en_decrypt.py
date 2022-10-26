d4d7680fe7a0b039c9d0e5467d9e7c4ccce1c044
maxblenner <maxb@live.ie>
2022-10-26 15:24:07 +0100

    First commit

---
+++ en_decrypt.py
@@ -0,0 +1,39 @@
+from cryptography.fernet import Fernet
+
+
+
+def create_key(filename):
+       
+    filepath = filename
+    key = Fernet.generate_key()
+    # Creates a new file
+    with open(filepath, 'wb') as f:
+        f.write(key)
+
+create_key("key.txt")
+
+
+""" 
+def load_key(key_file):    
+    try:
+        filename =  key_file 
+        with open(filename, 'rb') as f:
+            key = f.read()
+            return key
+        
+    except:        
+        print("No local key file found!, key created")        
+        return None
+    
+def encrypt_password(password,key_file):    
+    key = load_key(key_file)
+    f = Fernet(key)
+    encrypted = f.encrypt(password.encode())
+    return encrypted.decode('ascii')
+
+def decrypt_password(encrypted_pw,key_file):
+    key = load_key(key_file).decode('ascii')   
+    f = Fernet(key)    
+    decrypted = f.decrypt(encrypted_pw)
+    return decrypted.decode('ascii')
+"""
---
+++ masterPass.py
@@ -0,0 +1,35 @@
+from base64 import encodebytes
+from encodings import utf_8
+import bcrypt
+import hashlib
+
+def genSalt(): #using bcrypt to generate a random salt
+    salt = bcrypt.gensalt()
+    return salt
+
+def hashInput(userIn): #hashing input
+    hashed = hashlib.md5(userIn).hexdigest().encode()
+    return hashed
+
+def addSalt(userIn, salt): #concatenate user in with salt    
+    salted = userIn + salt 
+    hashed = hashInput(salted) #.encode()
+    return hashed.decode('ascii')
+
+def checkPassword(userIn, storedPW, salt): #checks user input against stored salted password by salting the password with the same salt and comparing the results
+    saltedIn = addSalt(userIn, salt)
+    
+    if saltedIn == storedPW:
+        print("Correct Password!")
+        return True
+    else:
+        print("Incorrect Password.")
+        return False
+
+salt = genSalt()
+print(salt)
+userIn = input("Enter Password: ").encode()
+print(userIn)
+hashed = addSalt(userIn,salt)
+print(hashed)
+
