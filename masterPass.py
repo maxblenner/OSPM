62aeb4317f0fb5e4f5c14e3bd79c35e971a97807
maxblenner <maxb@live.ie>
2022-10-27 16:20:04 +0100

    Second commit, updated the masterpass.py file with the intention of making something like a -dual key system- (one key is the master password and the other is the locally stored key [or salt as named in the file])

--- masterPass.py
+++ masterPass.py
@@ -16,6 +16,24 @@ def addSalt(userIn, salt): #concatenate user in with salt
     hashed = hashInput(salted) #.encode()
     return hashed.decode('ascii')
 
+def genSaltFile(filename): #writing salt to a file for persistence
+    filepath = filename
+    salt = genSalt()
+    # Creates a new file
+    with open(filepath, 'wb') as f:
+        f.write(salt)
+
+def loadSalt(filename): #loads the local salt (key)
+    try:
+        filename =  filename 
+        with open(filename, 'rb') as f:
+            salt = f.read()
+            return salt
+        
+    except:        
+        print("No local key file found!, key created")        
+        return None
+
 def checkPassword(userIn, storedPW, salt): #checks user input against stored salted password by salting the password with the same salt and comparing the results
     saltedIn = addSalt(userIn, salt)
     
@@ -26,10 +44,14 @@ def checkPassword(userIn, storedPW, salt): #checks user input against stored sal
         print("Incorrect Password.")
         return False
 
-salt = genSalt()
-print(salt)
-userIn = input("Enter Password: ").encode()
-print(userIn)
-hashed = addSalt(userIn,salt)
+
+#genSaltFile("salt.txt")
+
+masterPass = input("Enter Password Master Password: ").encode()
+print(masterPass)
+
+
+
+hashed = addSalt(masterPass,loadSalt("salt.txt"))
 print(hashed)
 
