a411646d8d66046f70c1636c737dc7645f3c81e4
maxblenner <maxb@live.ie>
2022-11-19 19:29:12 +0000

    put verification in a function

--- DBFunctions.py
+++ DBFunctions.py
@@ -7,12 +7,44 @@ conn = sqlite3.connect('OSPM.db') #connects to or constructs the database
 c = conn.cursor()
 
 def insertUser(user):
+    salt = KDF.genSalt()
     with conn: 
        c.execute("INSERT INTO Users VALUES (:ID, :Username, :Password, :Salt)", {'ID': user.id, 'Username': user.username, 'Password': user.password, 'Salt': user.salt}) 
 
 def insertAccount(account):
     with conn: 
        c.execute("INSERT INTO Accounts VALUES (:UserID, :ServiceName, :Login, :Password, :Note)", {'UserID': account.uid, 'ServiceName': account.serName, 'Login': account.login, 'Salt': account.salt, 'Note': account.note})
+
+def verifyLogin():
+    userIn = input("Username: ")
+    print(userIn)
+    userIn2 = input("Password: ").encode()
+    print("Verifying...")
+
+    #c = conn.cursor()
+    try:
+        c.execute("SELECT Password from Users WHERE Username=?",(userIn,))
+        password = c.fetchone()[0]
+        print(password)
+
+        c.execute("SELECT Salt from Users WHERE Username=?",(userIn,))
+        output = c.fetchone()[0] 
+        print(output)
+
+        attempt = KDF.deriveKey(userIn2,output)
+
+        if attempt == password:
+            print("Correct password!")
+            return True
+
+        else:
+            print("Incorrect credentials.")
+            return False
+    
+    except:
+        print("Error!")
+        return False
+
 
 #this is a test addition
 '''
@@ -32,25 +64,11 @@ insertUser(user2)
 c.execute("SELECT * FROM Users")
 print(c.fetchall())
 
+verifyLogin()
 
 
-print("Verify password for user1...")
-c.execute("SELECT Salt from Users WHERE ID=1")
-output = c.fetchone()[0]
-conn.commit()
-userIn = input("password = ").encode()
-
-attempt = KDF.deriveKey(userIn,output)
-
-c.execute("SELECT Password from Users WHERE ID=1")
-output = c.fetchone()[0]
-
-if attempt == output:
-    print("Correct password!")
-
-else:
-    print("Incorrect credentials")
-
+#user2 = User(2,'user2',userIn)
+#IDEA= Automate id selection by selecting the IDs from table and choosing n+1
 
 
 
